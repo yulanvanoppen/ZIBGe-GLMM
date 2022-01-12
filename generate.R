523fdfc5b217171c961d9fc2a234aa390f275875
Yulan <yulanvanoppen@gmail.com>
2022-01-12 10:52:58 +0100

    Tuned data generating script to match a simulation study case

--- generate.R
+++ generate.R
@@ -1,9 +1,15 @@
 library(Rcpp)
-sourceCpp('dZIBGe.cpp')                          # load ZIBG PMF
+sourceCpp('dZIBGe.cpp')                         # load ZIBG PMF
 
-load('rZIBGe.rda')                               # load ZIBG data-generating functions
+load('rZIBGe.rda')                              # load ZIBG data-generating functions
+
+M <- 4; N <- 2; th <- .5;                       # set distribution parameters
+pi1 <- .1; pi2 <- .05; pi3 <- .025
+
+mu <- ((2*(1 - pi1 - pi3))^(1/(M+1)) - 1)^-1    # reparameterize in terms of marginal means
+nu <- ((2*(1 - pi1 - pi2))^(1/(N+1)) - 1)^-1
 
 set.seed(0)
-y <- rZIBGe(200, c(5, 5, 0.5, 0.1, 0.05, 0.025)) # example: generate 200 data points
+y <- rZIBGe(200, c(mu, nu, th, pi1, pi2, pi3))  # generate 200 data points
 
 save(y, file='generated.rda')                   # store data
